6d6641d48350a52beef025345556c44224f8ccdd
Krampus <73042852+Krampus-update@users.noreply.github.com>
2025-12-01 21:05:43 -0300

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "krampus-update.github.io/O-Principe-de-Vidro/",
+    baseUrl: "krampus-update.github.io/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
